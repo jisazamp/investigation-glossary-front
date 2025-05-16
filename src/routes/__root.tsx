d07285009b3b508e723cc0cddb1d807280f5c4cb
Juan Isaza <juanisaza@Juans-Mac-mini.local>
2025-05-16 18:29:52 -0500

    add bootstrap remove tailwind

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,20 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../components/Header'
-
-import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
-
-import type { QueryClient } from '@tanstack/react-query'
+import type { QueryClient } from "@tanstack/react-query";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Header from "../components/Header";
+import TanStackQueryLayout from "../integrations/tanstack-query/layout.tsx";
 
 interface MyRouterContext {
-  queryClient: QueryClient
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Header />
-
       <Outlet />
       <TanStackRouterDevtools />
-
       <TanStackQueryLayout />
     </>
   ),
-})
+});
